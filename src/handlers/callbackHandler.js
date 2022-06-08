34447440e0f7420c87696ce74ff537cd4cc2a802
temacoder <temacoder@gmail.com>
2022-06-08 14:48:35 +0200

    Update for start project

--- src/handlers/callbackHandler.js
+++ src/handlers/callbackHandler.js
@@ -5,57 +5,6 @@ module.exports.CallbackHandler = (bot, db) => {
   bot.on('callback_query', async (ctx) => {
     const { callbackQuery, session } = ctx
 
-    if (callbackQuery.data === 'about-course') {
-      await ctx.editMessageText(messages['about-course'], InlineKeyboard.back())
-    }
 
-    if (callbackQuery.data === 'back') {
-
-      if (session.step === 'end') {
-        await ctx.replyWithHTML(messages.start, InlineKeyboard.start())
-      } else {
-        await ctx.editMessageText(messages.start, InlineKeyboard.start())
-      }
-
-    }
-
-    if (callbackQuery.data === 'signup') {
-      session.step = 'fio'
-      await ctx.editMessageText(messages.fio)
-    }
-
-    if (callbackQuery.data === 'card') {
-      session.step = 'end'
-      session.payments = 'Банковская карта'
-      await ctx.deleteMessage()
-      await ctx.replyWithHTML(`${messages.end}\n\n<b>Ваши данные:</b>\nВаше имя: ${session.name}\nВаш номер телефона: ${session.phone}\nВаш email: ${session.email}\nСпособ оплаты: ${session.payments}`, InlineKeyboard.back())
-
-      //Записываем данные юзера в базу даных
-      db.set('Object', {
-        [ctx.chat.id]: {
-          name: session.name,
-          phone: session.phone,
-          email: session.email,
-          payments: session.payments
-        }
-      })
-    }
-
-    if (callbackQuery.data === 'crypto') {
-      session.step = 'end'
-      session.payments = 'Криптовалюта'
-      await ctx.deleteMessage()
-      await ctx.replyWithHTML(`${messages.end}\n\n<b>Ваши данные:</b>\nВаше имя: ${session.name}\nВаш номер телефона: ${session.phone}\nВаш email: ${session.email}\nСпособ оплаты: ${session.payments}`, InlineKeyboard.back())
-
-      //Записываем данные юзера в базу даных
-      db.set('Object', {
-        [ctx.chat.id]: {
-          name: session.name,
-          phone: session.phone,
-          email: session.email,
-          payments: session.payments
-        }
-      })
-    }
   })
 }
--- src/handlers/textHandler.js
+++ src/handlers/textHandler.js
@@ -6,25 +6,7 @@ module.exports.TextHandler = (bot, db) => {
     const { session } = ctx
     const text = ctx.message.text
 
-    if (session.step === 'fio') {
 
-      session.step = 'phone'
-      session.name = text
-      await ctx.replyWithHTML(messages['phone-number'])
-
-    } else if (session.step === 'phone') {
-
-      session.step = 'email'
-      session.phone = text
-      await ctx.replyWithHTML(messages.email)
-
-    } else if (session.step === 'email') {
-
-      session.step = 'payments'
-      session.email = text
-      await ctx.replyWithHTML(messages.payments, InlineKeyboard.payments())
-
-    }
 
   })
 
